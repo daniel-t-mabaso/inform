a556875c49fcf7e40be8650f655df7ed7540cd04
unknown <mabasodaniel@gmail.com>
2019-08-26 15:03:50 +0200

    changed files into php files and added session for login tracking.

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php include("assets/php/session.php");
+
+if($_SESSION['auth']!= true){
+    
+    header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+    <body style="background-color: skyblue; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; ">
+    
+    </body>
+</html>

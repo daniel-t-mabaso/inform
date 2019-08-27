b2ad9ea3caa2f7c15260fe5ca578564647a2c2f5
Motshabi Shuping <SHPMOT001@myuct.ac.za>
2019-08-27 17:28:21 +0200

    Rectified errors and started working on home page and the profile page

--- index.php
+++ index.php
@@ -5,9 +5,22 @@ if($_SESSION['auth']!= true){
     header("Location: login.php");
 }
 ?>
+
+<?php
+            include 'assets/php/dependencies.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <body style="background-color: skyblue; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; ">
-    
+    <form method="POST">
+    <input type="submit" value="Logout" class="button center-txt" name="LOGOUT" >
+    </form>
+    <?php include("assets/php/session.php");
+       if(isset($_POST['LOGOUT']))
+       { 
+        session_destroy();
+       }
+       ?> 
     </body>
 </html>

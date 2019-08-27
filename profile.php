e44be9f52116ba5eefdb19911340fa8a559d505b
unknown <mabasodaniel@gmail.com>
2019-08-27 18:10:18 +0200

    Made a few changes to profile so that it pulls data from logged in user

--- profile.php
+++ profile.php
@@ -1,8 +1,13 @@
+<?php
+    include("assets/php/session.php");
+    include("assets/php/class_lib.php");
+?>
 <!Doctype html>
 <html>
 <head>
         <?php
             include 'assets/php/dependencies.php';
+            $user = unserialize($_SESSION['user']);
         ?>
     <body>
         <div style="background-color: skyblue; text-align: center">
@@ -12,23 +17,38 @@
         </div>
        
         <div class='center-txt padding-20 vertical-padding-30 max-width'>
-            <form method="post" action="./" class="center-txt vertical-padding-10">
-                Full Name:<br>
-                        <input required type="text" class="inputField" name="fullName" placeholder="Mickey Mouse" >
-                        <br>
-                Email:<br>
-                        <input required type="email" class="inputField" name="email" placeholder="mickeyMouse@gmail.com">
-                        <br>
-                Community Zip Code:<br>
-                        <input type="text" class="inputField" style="width: 91%" name="zipCode" required>
+            <form method="post" action="" class="left-txt vertical-padding-10">
+                Full Name
+                        <?php 
+                        $name = $user->get_full_name(); 
+                        echo "<input type='text' class='inputField' name='fullName' placeholder='$name'/>";?>
+                        
+                Email
+                        <?php
+                        $email = $user->get_email(); 
+                        echo "<input type='email' class='inputField' name='fullName' placeholder='$email'/>";?>
+                        
+                Community Zip Code
+                        <input type="text" class="inputField" name="zipCode">
                         <datalist>
                         </datalist>
-            </form>
-            <div class="book center-txt">
-                <input type="checkbox" name="newCommunity">New Community<br>
+            
+                        <div class="book center-txt">
+                <input class="book" type="checkbox" id='new-community-checkbox' onclick="document.getElementById('new-community-panel').classList.toggle('hide');" name="newCommunity">New Community<br>
+                <div class="hide  left-txt" id="new-community-panel">
+                    Zip Code
+                    <input type="text" class="inputField" name="newCommunityZip" placeholder="0000"/>
+                    Suburb
+                    <input type="text" class="inputField" name="newCommunityName" placeholder="Suburb Name"/>
+                </div>
             </div>
-            <input class="button center-txt" type="submit" value="Save" name="save" />   
-            <input class="button center-txt" style="background-color: grey;" type="submit" value="Deactivate Account" name="deleteProfile" /> 
+            <div class="center-txt">
+            <input class="button" type="submit" value="Save" name="save" />   
+            <input class="button" style="background-color: grey;" type="submit" value="Deactivate Account" name="deleteProfile" />
+        </div>
+                    
+        </form>
+            
         </div>
     </body>
 </html>

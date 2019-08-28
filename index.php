6c2ba66306ea35099e7f93f1405f07bcf5804647
Motshabi Shuping <SHPMOT001@myuct.ac.za>
2019-08-28 13:42:00 +0200

    Home page finished-minor touches left

--- index.php
+++ index.php
@@ -17,8 +17,12 @@ if($_SESSION['auth']!= true){
     ?>
     <title>HOME</title>
 </head>
-
-    <body style="background-color: skyblue; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; ">
+<div class="vertical-padding-40 white-txt" style="background-color: skyblue; text-align: center">
+        <h1 class="title center-txt bold">HOME</h1>
+     </div>
+<div style="background-color:white">
+<body style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; ">
+    <div class='center-txt padding-20 vertical-padding-10 max-width'>
     <div class="sideMenu">
     <a href="index.php">Home</a>
     <br>
@@ -34,16 +38,14 @@ if($_SESSION['auth']!= true){
     <br>
     </div>
     <div class="bodyHome">
-        <form method="post" action="" name="choice" class="right-txt vertical-padding-10">
-            <div class="small-width center hide-overflow">
-                <img class="max-width max-height" src='assets/media/images/event-icon.jpg'>
-            </div>
-            <input class="button" type="submit" value="Events" name="events" />
-            <div class="small-width center hide-overflow">
-                <img class="max-width max-height" src='assets/media/images/alert-icon.png'>
-            </div>
-            <input class="button" type="submit" value="Alerts" name="alerts" />
+        <form method="post" action="" name="choice">
+            <img class="small-width small-height" src='assets/media/images/event-icon.jpg'>
+            <input class="button left-txt" type="submit" value="Events" name="events" />
+            <br><br><br><br>
+            <img class="small-width small-height" src='assets/media/images/alert-icon.png'>
+            <input class="button center-txt" type="submit" value="Alerts" name="alerts" />
         </form>
+        </body>
     </div>
-    </body>
+</div>
 </html>
